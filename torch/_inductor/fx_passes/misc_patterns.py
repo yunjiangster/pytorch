Add a register_replacement to fix float8 delayed scaling kernel fusion issues
Summary:
We previously tried the `defer_reduction_split_after_fusion` way to fix the fusion issue. 
However, as we agree that the longer-term solution is cooperative reduction + tiled reduction, the defer reduction split approach will also be a shorter-term solution. And we want to keep the shorter-term solution simpler. 

This pr uses the `pattern_matcher` to match the fp8 delayed scaling pattern, simply replace every `max(abs(x))` with `max(abs(x), dim=-1), max()`. It generates the same result as the defer reduction split approach .

Test Plan:
Run float8 training script. Amax and cast are fused in delayed scaling; dynamic scaling is not affected.

The delayed scaling kernel also looks reasonable to me, https://fburl.com/phabricator/iqmlollk

```
TORCH_LOGS="fusion" TORCHINDUCTOR_LOOP_ORDERING_AFTER_FUSION=1  buck run mode/opt scripts/shuqiyang/test_inductor:test_float8 --  ~/local/tmp/20241120_test --dtype_filter float8 --scaling_type_input delayed --scaling_type_weight delayed --scaling_type_grad_output delayed  2>&1 | tee ~/test_compile.txt
```

```
TORCH_LOGS="fusion" TORCHINDUCTOR_LOOP_ORDERING_AFTER_FUSION=1  buck run mode/opt scripts/shuqiyang/test_inductor:test_float8 --  ~/local/tmp/20241120_test --dtype_filter float8 --scaling_type_input dynamic --scaling_type_weight dynamic --scaling_type_grad_output dynamic  2>&1 | tee ~/test_compile.txt
```

Differential Revision: D67135795

torch/_inductor/fx_passes/misc_patterns.py
@@ -8,7 +8,7 @@ from torch._ops import OpOverload, OpOverloadPacket
 from torch.utils._ordered_set import OrderedSet
 
 from ..pattern_matcher import fwd_only, register_replacement
-
+from ..utils import partialize_and_update_signature
 
 aten = torch.ops.aten
 
@@ -69,6 +69,52 @@ def _misc_patterns_init():
         scalar_workaround={"slice_shape": 42},
     )
 
+    # Float8 training patterns
+    E4M3_MAX_POS = torch.finfo(torch.float8_e4m3fn).max
+    E5M2_MAX_POS = torch.finfo(torch.float8_e5m2).max
+
+    def amax_with_scaling_pattern(tensor_x_inp, scale_x, IS_BWD):
+        tensor_x = tensor_x_inp.to(torch.float32) * scale_x
+        if IS_BWD:
+            tensor_x = tensor_x.clamp(min=-1 * E5M2_MAX_POS, max=E5M2_MAX_POS)
+            tensor_x = tensor_x.to(torch.float8_e5m2)
+        else:
+            tensor_x = tensor_x.clamp(min=-1 * E4M3_MAX_POS, max=E4M3_MAX_POS)
+            tensor_x = tensor_x.to(torch.float8_e4m3fn)
+        amax = torch.max(torch.abs(tensor_x_inp))
+        return (tensor_x, amax)
+
+    def amax_with_scaling_tiled_replacement(tensor_x_inp, scale_x, IS_BWD):
+        tensor_x = tensor_x_inp.to(torch.float32) * scale_x
+        if IS_BWD:
+            tensor_x = tensor_x.clamp(min=-1 * E5M2_MAX_POS, max=E5M2_MAX_POS)
+            tensor_x = tensor_x.to(torch.float8_e5m2)
+        else:
+            tensor_x = tensor_x.clamp(min=-1 * E4M3_MAX_POS, max=E4M3_MAX_POS)
+            tensor_x = tensor_x.to(torch.float8_e4m3fn)
+        amax_1 = torch.max(torch.abs(tensor_x_inp), dim=-1).values
+        amax = torch.max(amax_1)
+        return (tensor_x, amax)
+    
+    # The amax_with_scaling_pattern will also match dynamic scaling cases, we want to avoid that.
+    # `scale_x` of delayed scaling comes from the previous iteration (a `primals`), instead of from a function call.
+    def fp8_delayed_scaling_extra_check(match):
+        return match.kwargs["scale_x"].op != "call_function"
+
+    if torch.cuda.is_available():
+        for IS_BWD in [True, False]:
+            # torch.bfloat16 has the same pattern as torch.float16, because they both needs `tensor_x_inp.to(torch.float32)` 
+            # Putting both `torch.bfloat16` and `torch.float16`patterns will cause errors in `assert pattern_repr not in _seen_patterns`
+            for dtype in [torch.float32, torch.float16]: 
+                device = "cuda"
+                register_replacement(
+                    partialize_and_update_signature(amax_with_scaling_pattern, IS_BWD=IS_BWD),
+                    partialize_and_update_signature(amax_with_scaling_tiled_replacement, IS_BWD=IS_BWD),
+                    [torch.tensor((16, 16), device=device, dtype=dtype), torch.tensor(2.0, device=device, dtype=torch.float32)],
+                    fwd_only,
+                    joint_graph_patterns,
+                    extra_check=fp8_delayed_scaling_extra_check,
+                )
 
 class NumpyCompatNormalization:
     numpy_compat: Dict[str, Tuple[str, ...]] = {