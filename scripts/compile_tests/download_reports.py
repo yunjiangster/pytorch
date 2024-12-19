93c7b1b98a494cf4eac777cbf43e76581a9fea99
IvanKobzarev <ivan.kobzarev@gmail.com>
2024-12-19 08:57:03 -0800

    [tests] TEST_WITH_SUBCLASSES env var
    ghstack-source-id: c4642cb25d87bacc2119a4ee181305a0afe506b1
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/134681

--- scripts/compile_tests/download_reports.py
+++ scripts/compile_tests/download_reports.py
@@ -13,6 +13,16 @@ CONFIGS = {
         "linux-focal-py3.9-clang10 / test (dynamo_wrapped, 2, 3, linux.2xlarge)",
         "linux-focal-py3.9-clang10 / test (dynamo_wrapped, 3, 3, linux.2xlarge)",
     },
+    "aot_eager39": {
+        "linux-focal-py3.9-clang10 / test (aot_eager_wrapped, 1, 3, lf.linux.12xlarge)",
+        "linux-focal-py3.9-clang10 / test (aot_eager_wrapped, 2, 3, lf.linux.12xlarge)",
+        "linux-focal-py3.9-clang10 / test (aot_eager_wrapped, 3, 3, lf.linux.12xlarge)",
+    },
+    "subclasses39": {
+        "linux-focal-py3.9-clang10 / test (subclasses_wrapped, 1, 3, lf.linux.12xlarge)",
+        "linux-focal-py3.9-clang10 / test (subclasses_wrapped, 2, 3, lf.linux.12xlarge)",
+        "linux-focal-py3.9-clang10 / test (subclasses_wrapped, 3, 3, lf.linux.12xlarge)",
+    },
     "dynamo311": {
         "linux-focal-py3.11-clang10 / test (dynamo_wrapped, 1, 3, linux.2xlarge)",
         "linux-focal-py3.11-clang10 / test (dynamo_wrapped, 2, 3, linux.2xlarge)",
@@ -22,6 +32,16 @@ CONFIGS = {
         "linux-focal-py3.11-clang10 / test (default, 1, 3, linux.2xlarge)",
         "linux-focal-py3.11-clang10 / test (default, 2, 3, linux.2xlarge)",
         "linux-focal-py3.11-clang10 / test (default, 3, 3, linux.2xlarge)",
+    },
+    "aot_eager311": {
+        "linux-focal-py3.11-clang10 / test (aot_eager_wrapped, 1, 3, linux.12xlarge)",
+        "linux-focal-py3.11-clang10 / test (aot_eager_wrapped, 2, 3, linux.12xlarge)",
+        "linux-focal-py3.11-clang10 / test (aot_eager_wrapped, 3, 3, linux.12xlarge)",
+    },
+    "subclasses311": {
+        "linux-focal-py3.11-clang10 / test (subclasses_wrapped, 1, 3, linux.12xlarge)",
+        "linux-focal-py3.11-clang10 / test (subclasses_wrapped, 2, 3, linux.12xlarge)",
+        "linux-focal-py3.11-clang10 / test (subclasses_wrapped, 3, 3, linux.12xlarge)",
     },
 }
 
@@ -51,6 +71,7 @@ def download_reports(commit_sha, configs=("dynamo39", "dynamo311", "eager311")):
     output = subprocess.check_output(
         ["gh", "run", "list", "-c", commit_sha, "-w", "pull", "--json", "databaseId"]
     ).decode()
+    print(f"XXX download_reports output:{output}")
     workflow_run_id = str(json.loads(output)[0]["databaseId"])
     output = subprocess.check_output(["gh", "run", "view", workflow_run_id])
     workflow_jobs = parse_workflow_jobs(output)
@@ -61,6 +82,7 @@ def download_reports(commit_sha, configs=("dynamo39", "dynamo311", "eager311")):
     required_jobs = []
     for config in configs:
         required_jobs.extend(list(CONFIGS[config]))
+
     for job in required_jobs:
         assert (
             job in workflow_jobs
@@ -73,6 +95,7 @@ def download_reports(commit_sha, configs=("dynamo39", "dynamo311", "eager311")):
 
     def download_report(job_name, subdir):
         job_id = workflow_jobs[job_name]
+
         for listing in listings:
             name = listing["name"]
             if not name.startswith("test-reports-"):
